23c01a6e668eb1b4ce0b63a2d50f07ebfd6a02f9
Kanaka Navya Sri <kanakanandhini439@gmail.com>
2026-07-20 10:47:02 +0530

    Implemented POST mapping

--- src/main/java/com/navya/simplewebapp/controller/productcontroller.java
+++ src/main/java/com/navya/simplewebapp/controller/productcontroller.java
@@ -18,20 +18,20 @@ public class productcontroller {
           return service.getProducts();
      }
 
-     @GetMapping("/product/{proid}")
-     public product getproductbyid(@PathVariable int proid) {
-          return service.getproductbyid(proid);
-     }
-}
+//     @GetMapping("/product/{proid}")
+//     public product getproductbyid(@PathVariable int proid) {
+//          return service.getproductbyid(proid);
+//     }
+//}
 //     @RequestMapping("/product/{proname}")
 //     public product getproductbyname(@PathVariable String proname){
 //          return service.getproductbyname(proname);
 //     }
-//     @PostMapping("/product")
-//public void addproduct(@RequestBody product prod){
-//          System.out.println(prod);
-//           service.addproduct(prod);
-//}
+     @PostMapping("/product")
+public void addproduct(@RequestBody product prod){
+          System.out.println(prod);
+           service.addproduct(prod);
+}
 //@PutMapping("/product")
 //public void updateproduct(@RequestBody product prod){
 //          service.updateproduct(prod);
