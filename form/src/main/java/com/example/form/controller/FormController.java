2995074fe8c8b768858cab642d8d1a7e5184602e
dhivya <dhivyasakthi29@gmail.com>
2026-07-20 11:48:00 +0530

    delete changes modified

--- form/src/main/java/com/example/form/controller/FormController.java
+++ form/src/main/java/com/example/form/controller/FormController.java
@@ -2,11 +2,19 @@ package com.example.form.controller;
 
 import java.util.List;
 
-import com.example.form.entity.Form;
-import com.example.form.service.FormService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.form.entity.Form;
+import com.example.form.service.FormService;
 
 
 @RestController
