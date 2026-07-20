7200726f43864edcea360c9551fd53c9619ae49d
Kanaka Navya Sri <kanakanandhini439@gmail.com>
2026-07-20 11:33:36 +0530

    updated

--- src/main/java/com/navya/simplewebapp/controller/productcontroller.java
+++ src/main/java/com/navya/simplewebapp/controller/productcontroller.java
@@ -36,8 +36,8 @@ public class productcontroller {
 //public void updateproduct(@RequestBody product prod){
 //          service.updateproduct(prod);
 //}
-@DeleteMapping("/product/{proid}")
-public void deleteproduct(@PathVariable int proid){
+@DeleteMapping("/product/{proname}")
+public void deleteproduct(@PathVariable int proname){
           service.deleteproduct(proid);
 }
 //
