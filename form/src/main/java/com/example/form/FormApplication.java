dc01942c5fb90544520a3baa36d891a4a77aa2a4
dhivya <dhivyasakthi29@gmail.com>
2026-07-20 11:07:06 +0530

    form updated

--- form/src/main/java/com/example/form/FormApplication.java
+++ form/src/main/java/com/example/form/FormApplication.java
@@ -8,7 +8,7 @@ public class FormApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(FormApplication.class, args);
+		SpringApplication.run(FormApplication.class,args);
 	}
 
 }
