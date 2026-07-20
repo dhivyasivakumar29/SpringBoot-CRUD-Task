0396eddd2529951488047f045d80783017b90cbb
dhivya <dhivyasakthi29@gmail.com>
2026-07-20 11:14:02 +0530

    cors updated

--- form/src/main/java/com/example/form/controller/FormController.java
+++ form/src/main/java/com/example/form/controller/FormController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/form")
-@CrossOrigin(origins="http://localhost:5173")
 public class FormController {
 
     @Autowired
